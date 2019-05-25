382412ca01444ed9e57a78374bd1a45b6e570ee0
houfei <331488738@qq.com>
2019-05-25 19:02:49 +0800

    添加事物管理器-----

--- src/com/auke/spring/AccountServiceImpl.java
+++ src/com/auke/spring/AccountServiceImpl.java
@@ -1,5 +1,9 @@
 package com.auke.spring;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
 public class AccountServiceImpl implements AccountService {
     /**
      *  注入Dao层类
@@ -11,15 +15,29 @@ public class AccountServiceImpl implements AccountService {
     }
 
     /**
+     *  注入事物管理的模板
+     */
+    private TransactionTemplate transactionTemplate;
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    /**
      *  转账方法
      * @param out
      * @param in
      * @param money
      */
     @Override
-    public void transfer(String out, String in, Double money) {
-        accountDao.outMoney(out, money);
-        int i = 1/0;
-        accountDao.inMoney(in, money);
+    public void transfer(final String out, final String in, final Double money) {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                accountDao.outMoney(out, money);
+                int i = 1/0;
+                accountDao.inMoney(in, money);
+            }
+        });
     }
 }
